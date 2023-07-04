60550df5eef193e173232bb3b3d9d767818c0ed4
Ist64 <v@email.com>
2023-07-04 18:44:04 +0500

    change log 2

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
 const center = document.querySelector(".center");
 console.log(center);
 
-center.innerHTML = "<h1>My first git project!!!</h1>";
+center.innerHTML = "<h1>My first git projectdd!!!</h1>";
